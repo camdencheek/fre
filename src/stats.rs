4b274cc877e784825a5f5efb36b0dc574c48da70
Camden Cheek <ccheek22@gmail.com>
2018-10-07 18:54:48 -0400

    Clarify tests

--- src/stats.rs
+++ src/stats.rs
@@ -60,6 +60,7 @@ impl PathStats {
     pub fn get_frecency(&self) -> f32 {
         self.frecency / 2.0f32.powf(
           self.secs_since_access() as f32 / self.half_life)
+          
     }
 
     pub fn update_last_access(&mut self) {
@@ -105,31 +106,17 @@ mod tests {
   use super::*;
   use spectral::prelude::*;
 
-  fn create_low_path() -> PathStats {
+  fn create_path() -> PathStats {
     let test_path = "/test/path".to_string();
     let ref_time = current_time_secs();
 
     PathStats {
-      half_life: 10.0,
+      half_life: 1.0,
       reference_time: ref_time,
       path: test_path.clone(),
-      frecency: 1.0,
-      last_accessed: 100.0,
-      num_accesses: 1,
-    }
-  }
-
-  fn create_high_path() -> PathStats {
-    let test_path = "/test/path".to_string();
-    let ref_time = current_time_secs();
-
-    PathStats {
-      half_life:10.0,
-      reference_time: ref_time,
-      path: test_path.clone(),
-      frecency: 2.0,
-      last_accessed: 200.0,
-      num_accesses: 2,
+      frecency: 0.0,
+      last_accessed: 0.0,
+      num_accesses: 0,
     }
   }
 
@@ -148,8 +135,12 @@ mod tests {
 
   #[test]
   fn compare_with_func() {
-    let low_path_stats = create_low_path();
-    let high_path_stats = create_high_path();
+    let low_path_stats = create_path();
+    let mut high_path_stats = create_path();
+
+    high_path_stats.frecency = 1.0;
+    high_path_stats.last_accessed = 1.0;
+    high_path_stats.num_accesses = 1;
 
     assert_eq!(Ordering::Less, low_path_stats.cmp_frecent(&high_path_stats));
     assert_eq!(Ordering::Less, low_path_stats.cmp_recent(&high_path_stats));
@@ -159,8 +150,12 @@ mod tests {
 
   #[test]
   fn compare_with_enum() {
-    let low_path_stats = create_low_path();
-    let high_path_stats = create_high_path();
+    let low_path_stats = create_path();
+    let mut high_path_stats = create_path();
+
+    high_path_stats.frecency = 1.0;
+    high_path_stats.last_accessed = 1.0;
+    high_path_stats.num_accesses = 1;
 
     assert_eq!(Ordering::Less, low_path_stats.cmp_score(&high_path_stats, &SortMethod::Frecent));
     assert_eq!(Ordering::Less, low_path_stats.cmp_score(&high_path_stats, &SortMethod::Recent));
@@ -170,39 +165,94 @@ mod tests {
 
   #[test]
   fn update_score() {
-    let mut low_path_stats = create_low_path();
+    let mut low_path_stats = create_path();
 
     low_path_stats.update_score(1.0);
 
-    assert_that!(low_path_stats.frecency).is_close_to(2.0,0.01);
-    assert_that!(low_path_stats.num_accesses).is_equal_to(1);
+    assert_that!(low_path_stats.frecency).is_close_to(1.0,0.01);
+    assert_that!(low_path_stats.num_accesses).is_equal_to(0);
   }
 
   #[test]
   fn update_num_accesses() {
-    let mut low_path_stats = create_low_path();
+    let mut low_path_stats = create_path();
 
     low_path_stats.update_num_accesses(1);
 
-    assert_that!(low_path_stats.num_accesses).is_equal_to(2);
-    assert_that!(low_path_stats.frecency).is_close_to(1.0,0.01);
+    assert_that!(low_path_stats.num_accesses).is_equal_to(1);
+    assert_that!(low_path_stats.frecency).is_close_to(0.0,0.01);
   }
 
   #[test]
   fn update_last_access() {
-    let mut low_path_stats = create_low_path();
+    let mut low_path_stats = create_path();
 
     low_path_stats.update_last_access();
 
-    assert_that!(low_path_stats.secs_since_access()).is_close_to(0.0, 0.01);
+    assert_that!(low_path_stats.secs_since_access()).is_close_to(0.0, 0.1);
   }
 
 
   #[test]
-  fn to_string() {
-    let low_path_stats = create_low_path();
+  fn to_string_no_stats() {
+    let low_path_stats = create_path();
 
     assert_that!(low_path_stats.to_string(&SortMethod::Frecent, false))
-      .is_equal_to("/test/path\n".to_string())
+      .is_equal_to("/test/path\n".to_string());
+    assert_that!(low_path_stats.to_string(&SortMethod::Recent, false))
+      .is_equal_to("/test/path\n".to_string());
+    assert_that!(low_path_stats.to_string(&SortMethod::Frequent, false))
+      .is_equal_to("/test/path\n".to_string());
+  }
+
+  #[test]
+  fn to_string_stats() {
+    let low_path_stats = create_path();
+
+    assert_that!(low_path_stats.to_string(&SortMethod::Frecent, true))
+      .is_equal_to("0.000\t/test/path\n".to_string());
+    assert_that!(low_path_stats.to_string(&SortMethod::Recent, true))
+      .is_equal_to("0.000\t/test/path\n".to_string());
+    assert_that!(low_path_stats.to_string(&SortMethod::Frequent, true))
+      .is_equal_to("0\t/test/path\n".to_string());
+
+  }
+
+  #[test]
+  fn get_frecency_one_half_life() {
+    let mut low_path_stats = create_path();
+
+    low_path_stats.frecency = 1.0;
+    low_path_stats.last_accessed = -1.0;
+
+    assert_that!(low_path_stats.get_frecency())
+      .is_close_to(0.5, 0.1);
+  }
+
+  #[test]
+  fn get_frecency_two_half_lives() {
+    let mut low_path_stats = create_path();
+
+    low_path_stats.frecency = 1.0;
+    low_path_stats.last_accessed = -2.0;
+
+    assert_that!(low_path_stats.get_frecency())
+      .is_close_to(0.25, 0.1);
+  }
+
+  #[test]
+  fn secs_since_access() {
+    let mut low_path_stats = create_path();
+
+    low_path_stats.last_accessed = -2.0;
+
+    assert_that!(low_path_stats.secs_since_access()).is_close_to(2.0,0.1);
+  }
+
+  #[test]
+  fn secs_elapsed_one_second() {
+    let one_second_ago = current_time_secs() - 1.0;
+
+    assert_that!(secs_elapsed(one_second_ago)).is_close_to(1.0, 0.1);
   }
 }
