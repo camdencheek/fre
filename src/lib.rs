2cf683b27c8917e9b0ba45eb2685662e27551fb7
zethra <jediben97@gmail.com>
2018-12-11 23:30:22 -0500

    Removed dependency duration_as_u128 so we are one step closer to stable
    Signed-off-by: zethra <jediben97@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-#![feature(duration_as_u128)]
 #![feature(test)]
 
 extern crate test;
@@ -24,7 +23,7 @@ pub enum SortMethod {
 /// Return the current time in seconds as a float
 pub fn current_time_secs() -> f64 {
     match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
-        Ok(n) => n.as_millis() as f64 / 1000.0,
+        Ok(n) => (n.as_secs() as u128 * 1000 + n.subsec_millis() as u128) as f64 / 1000.0,
         Err(e) => {
             error!("invalid system time: {}", e);
             process::exit(1);
